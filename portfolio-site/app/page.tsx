17fde1ec1a39ea15eeacba9a8de8481c37c6674b
Chance B. Loveday <141525952+cloveday098@users.noreply.github.com>
2026-05-31 21:47:35 -0400

    Decent start to head section of Home page; basic alignment & styling

--- portfolio-site/app/page.tsx
+++ portfolio-site/app/page.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
-import './homePage.css'
 
+/* Function Parameter Declarations */
 interface HeaderProps {
   name: string;
 }
@@ -9,36 +9,54 @@ interface TitleProps {
   title: string;
 }
 
-function Header({name}: HeaderProps) {
+/* Functions */
+function SectHeader({name}: HeaderProps) {
   return <h1 className="header">{name}</h1>;
 }
 
 function Title({title}: TitleProps) {
   return(
-  <header>
-    <h1 className="title">{title}</h1>
-    <h2>Sevierville, TN</h2>
-    <img src="static/email.jpg" alt="Email Logo" width="50" height="50"></img> <a href="lovedaychance@gmail.com">lovedaychance@gmail.com</a>
-    <img src="static/linkedin_logo.png" alt="LinkedIn Logo" width="50" height="50"></img> <a href="https://www.linkedin.com/in/chance-loveday-a941b4222/">https://www.linkedin.com/in/chance-loveday-a941b4222/</a>
-  </header>
+  <div>
+    <center>
+      <h1 className="title">{title}</h1>
+      <h2 className="subheader">Sevierville, TN</h2>
+    </center>
+    
+    <div className="row-container">
+      <div className="item-container">
+        <img src="static/email.jpg" alt="Email Logo" width="50" height="50"></img> <a href="lovedaychance@gmail.com">lovedaychance@gmail.com</a>
+      </div>
+
+      <div className="item-container">
+        <img src="static/linkedin_logo.png" alt="LinkedIn Logo" width="50" height="50"></img> <a href="https://www.linkedin.com/in/chance-loveday-a941b4222/">Chance Loveday</a>
+      </div>
+    </div>
+
+    <center><img src="static/Chance_Prof_Headshot.jpg" alt="Headshot" width="300" height="300"></img></center>
+  </div>
   );
 }
 
 
 export default function page() {
   return (
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <section>
+  <main className="body">
+    <section id="Bio">
       <Title title="Chance Loveday"/>
-      <img src="static/Chance_Prof_Headshot.jpg" alt="Headshot" width="500" height="500"></img>
+    </section>
 
-      <Header name="Mentorship"/>
+      <section id="mentorship">
+      <SectHeader name="Mentorship"/>
       <img src="static/S3_Chat_Aquarium_Conserv_Pic.jpg" alt="Group picture with the 2024 Scots Science Scholars cohort at the Chattanooga Aquarium Conservation" width="500" height="500"></img>
       <img src="static/S3_Tree_Group_Pic.png" alt="2024 S3 cohort at the end of orientation" width="500" height="500"></img>
+      </section>
 
-      <Header name="Teaching Assistance -- Move to CV Page"/>
+      <section>
+      <SectHeader name="Teaching Assistance -- Move to CV Page"/>
+      </section>
 
-      <Header name="Tutoring"/>
+      <section>
+      <SectHeader name="Tutoring"/>
       <img src="static/New Spring STEM Success Flyer.png" alt="STEM Success Flyer" width="300" height="400"></img>
       <img src="static/TRIO Tutoring wt Chance Loveday (1).png" alt="TRIO Tutor Flyer" width="300" height="400"></img>
     </section>
